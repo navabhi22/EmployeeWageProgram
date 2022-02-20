b5d4b587f7bb84692587b2a6ebd8840f24753490
navabhi22 <navabhi22@gmail.com>
2022-02-20 15:54:21 +0530

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace EmployeeWageProgram
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program ");
 
-            EmployeWageUsingSwitchCase.CalculateDailyWage();
+           WageForMonth.CalculateWageForMonth();
         }
     }
 }
