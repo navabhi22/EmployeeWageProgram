97afcb7f7a2666d04a6cda509e52b9200ae31ffc
navabhi22 <navabhi22@gmail.com>
2022-02-20 14:10:22 +0530

    Added Class to Check Employee is Present or Not

---
+++ CheckEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    public class CheckEmployee
+    {
+        public static void CheckEmployeePresentOrNot()
+        {
+            Random random = new Random();
+            int AttendanceVariable = random.Next(2);
+            int IsPresent = 1;
+
+            if (AttendanceVariable == IsPresent)
+
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+
+    }
+}
